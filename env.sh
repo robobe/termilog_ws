83a251a938686e77d92805d0c8e9ebd3b4dfce40
robo2020 <robo2020@gmail.com>
2025-01-30 06:42:23 +0200

    add env on terminal open

---
+++ env.sh
@@ -0,0 +1,10 @@
+source install/setup.bash
+
+function parse_git_dirty {
+  [[ $(git status --porcelain 2> /dev/null) ]] && echo "*"
+}
+function parse_git_branch {
+  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/ (\1$(parse_git_dirty))/"
+}
+
+export PS1="\n\H \[\033[32m\]\w\[\033[33m\]\$(parse_git_branch)\[\033[00m\] 🐢 $ "
